10a0aaa7fc858a754659bfd79175de008ad0a75b
kmsmahi <salauddinmahi2002@gmail.com>
2025-11-09 06:05:45 +0600

    second commit

--- api1.js
+++ api1.js
@@ -4,13 +4,34 @@ const lesson=()=>{
     .then((data)=>display(data))
     .catch((error)=>console.log(error));
 }
+const loadLesson=(id)=>{
+    const url=`https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>displayWords(data.data))
+    .catch((error)=>console.log(error));
+    
+};
+const displayWords=(words)=>{
+    const wordContainer=document.getElementById('word-container');
+    // wordContainer.innerHTML='';
+    words.forEach((word)=>{
+        console.log(word);
+        const wordDiv=document.createElement('div');
+        wordDiv.innerHTML=`
+        
+        `;
+        wordContainer.appendChild(wordDiv);
+    })
+}
+
 const display=(lessons)=>{
     const lessonContainer=document.getElementById('lesson-div');
     lessonContainer.innerHTML='';
     for(let lesson of lessons.data){
         const button=document.createElement('div');
         button.innerHTML=`
-        <button class="btn btn-soft btn-primary">lesson - ${lesson.level_no}</button>
+        <button onclick="loadLesson(${lesson.level_no})" class="btn btn-soft btn-primary">lesson - ${lesson.level_no}</button>
         `;
         lessonContainer.append(button);
     }
